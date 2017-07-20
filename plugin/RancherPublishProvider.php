424da4e77ab0c9f7b789116c27d600baabf8e46c
Sven Speckmaier <sps@ipunkt.biz>
2017-07-20 11:33:53 +0200

    plugin base infrastructure

---
+++ plugin/RancherPublishProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class RancherPublishProvider
+ */
+class RancherPublishProvider implements \Rancherize\Plugin\Provider {
+
+	use \Rancherize\Plugin\ProviderTrait;
+
+	/**
+	 */
+	public function register() {
+		// TODO: Implement register() method.
+	}
+
+	/**
+	 */
+	public function boot() {
+		// TODO: Implement boot() method.
+	}
+}
